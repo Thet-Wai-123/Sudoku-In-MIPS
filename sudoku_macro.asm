edaf518b79c4dbe1fbf8d392bedea32e23f3f2c8
Theoden Melgar <113568468+raglem@users.noreply.github.com>
2025-04-30 16:00:39 -0700

    updated get_value macro to move return value to $v0

--- sudoku_macro.asm
+++ sudoku_macro.asm
@@ -1,10 +1,10 @@
 # General use macros for sudoku board
-# Assume board will be stored at $s0
+# Assume we have access to board from data portion
 # Row and column are assumed to be integers stored in registers
 
 .macro get_value(%row, %column)
 	#move board address to $t0
-	move $t0, $s0
+	move $t0, board
 	
 	#multiply column by rowSize
 	#rowSize is 9
@@ -19,6 +19,6 @@
 	#add index to board address
 	add $t0, $t0, $t1
 	
-	#move cell address to $t1
-	move $s1, $t0
+	#move cell address to $v0
+	move $v0, $t0
 .end_macro
