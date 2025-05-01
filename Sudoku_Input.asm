4ca99f61cbc52ad75ce018a7d6f574ca159d0f8e
Nicholas Garcia <nickgarcia0523@gmail.com>
2025-04-30 23:04:03 -0700

    Update Sudoku_Input.asm

--- Sudoku_Input.asm
+++ Sudoku_Input.asm
@@ -4,53 +4,53 @@ prompt_row: .asciiz "Enter row  (1–9, 0 to exit): "
 prompt_col: .asciiz "Enter col  (1–9, 0 to exit): "
 prompt_val: .asciiz "Enter value(1–9, 0 to clear): "
 
-    # .macro is the getInt function — wrapper for syscall 5
-    .macro getInt dst
-        li   $v0, 5       # syscall: read integer
-        syscall
-        move \dst, $v0
-    .end_macro
-    # getInput uses the function (jal/jr)
-    # Will return:
-    # $v0 = row
-    # $v1 = col
-    # $v2 = val
-    .text
-    .globl getInput
+.text
+.globl getInput
 getInput:
-    # reads the row 
-    li   $v0, 4       # prints prompt_row
+    # Reads the row
+    li   $v0, 4
     la   $a0, prompt_row
     syscall
-    getInt $a0       # read into $a0
-    move $v0, $a0       # return the row to $v0
 
-    # optional: range check 0–9, otherwise reprompt if invalid
-    # reads the collumn
+    li   $v0, 5
+    syscall
+    move $v0, $v0   #returns the row in $v0
+
+    # Reads a colummn
     li   $v0, 4
     la   $a0, prompt_col
     syscall
-    getInt $a0       # read into $a0
-    move $v1, $a0       # returns thr col to $v1
 
-    # reads the value
+    li   $v0, 5
+    syscall
+    move $v1, $v0   #returns the col in $v1
+
+    # Reads the value
     li   $v0, 4
     la   $a0, prompt_val
     syscall
-    getInt $a0       # read into $a0
-    move $v2, $a0       # return the val function to $v2
 
-    jr   $ra       # sends back to the caller
+    li   $v0, 5
+    syscall
+    move $v2, $v0   #returns val in $v2
 
-
-
-
-
-
-
+    jr $ra
+    
+    
+    
+    
+    
+    
     ####### For the .text/main section: #######
-    jal  getInput
-    # So now: $v0=row, $v1=col, $v2=val
-    # checks for an exit condition, such as: beq $v0,$zero,exit, and more
-    # compute the index and stores into the grid as before
+    .text
+.globl main
+main:
+    jal getInput
+    #you can use $v0 (row), $v1 (col), $v2 (val)
+    # e.g., check for exit:
+    beq $v0, $zero, exit
+#More logic can go here if anythimg
 
+exit:
+    li $v0, 10
+    syscall
